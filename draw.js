af97929be5bf1f50afba99e11f60128a8045d3a3
andy <andrew@risertech.com>
2024-10-18 17:40:26 -0600

    made grid lines thiner

--- draw.js
+++ draw.js
@@ -3,6 +3,7 @@ let color = "black"
 let isRainbow = false
 let isShaded = false
 
+
 const newGrid = document.querySelector("#newGrid")
 newGrid.addEventListener("click", () => {
     prom = prompt("please give a number between 1 and 100", "30")
@@ -16,33 +17,33 @@ newGrid.addEventListener("click", () => {
     isRainbow = false
     isShaded = false
     color = "black"
-});
+})
 
 const eraser = document.getElementById("eraser")
 eraser.addEventListener("click", () => {
     isRainbow = false
     isShaded = false
     color = "white"
-});
+})
 
 const random = document.getElementById("rndm")
 random.addEventListener("click", () => {
     isRainbow = true
     isShaded = false
-});
+})
 
 const newColor = document.getElementById("newColor")
 newColor.addEventListener("input", (event) => {
     isRainbow = false
     isShaded = false
     color = event.target.value
-});
+})
 
 const shade = document.getElementById("shade")
 shade.addEventListener("click", () => {
     isShaded = true
     isRainbow = false
-});
+})
 
 function rainbow() {
     return '#' + Math.floor(Math.random() * 16777215).toString(16)
@@ -52,12 +53,12 @@ function createGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const content = document.createElement("div")
         content.style.cssText = "background: white"
-        content.style.border = '1px solid black'
+        content.style.border = `${1 / 10}px solid black`
         content.style.width = `calc(700px / ${size})`
         content.style.height = `calc(700px / ${size})`
         content.style.aspectRatio = "1/1"
         content.style.boxSizing = "border-box"
-        content.addEventListener("mouseenter", (event) => {
+        content.addEventListener("mouseenter", () => {
             if (isShaded) {
                 content.style.opacity = (parseFloat(content.style.opacity) || 0) + 0.1
             } else {
